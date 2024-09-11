6230b413df925e54e70340863c5e439e17009122
Alberto Cobo <alberto.cobo@aeorum.com>
2024-09-11 11:45:24 +0200

    Added missions on .env

--- interfaz/routes/indexManagement.js
+++ interfaz/routes/indexManagement.js
@@ -3,6 +3,7 @@ module.exports = function (Manager) {
     const express = require('express');
     const router = express.Router();
     const fleetManager = process.env.FLEET_EXCHANGE;
+    const missions = JSON.parse(process.env.MISSION_LIST.replace(/&quot;/g, '\"'))
   
     router.get('/', (req, res, next) => {
         res.redirect('/mapView');
@@ -82,76 +83,6 @@ module.exports = function (Manager) {
         res.status(400).json({ success: false, error: 'ERR_BADPARAMS' });
     }
 });
-
-
-    let missions = [
-      {id: 1, name: 'Lineal 3', missionType: 'linear',
-        structPoints: [
-        {
-          'type': 'waypoint',
-          'lat': 36.74662682607213,
-          'lon': -4.5539314358383365,
-          'alt': 20
-        },
-        {
-          'type': 'waypoint',
-          'lat': 36.74711463511929,
-          'lon': -4.553618091998146,
-          'alt': 20
-        },
-        {
-          'type': 'waypoint',
-          'lat': 36.747625755242055,
-          'lon': -4.553790431110286,
-          'alt': 20
-        }
-      ]},
-      {id: 2, name: 'Perimetral', missionType: 'perimeter',
-        structPoints: [
-        {
-          'type': 'waypoint',
-          'lat': 36.74658737079311,
-          'lon': -4.553846385367393,
-          'alt': 20
-        },
-        {
-          'type': 'waypoint',
-          'lat': 36.74708594055534,
-          'lon': -4.553512897994636,
-          'alt': 20
-        },
-        {
-          'type': 'waypoint',
-          'lat': 36.74745896904987,
-          'lon': -4.553685237106777,
-          'alt': 20
-        },
-        {
-          'type': 'waypoint',
-          'lat': 36.74764906941206,
-          'lon': -4.55405453520487,
-          'alt': 20
-        },
-        {
-          'type': 'waypoint',
-          'lat': 36.74731190995604,
-          'lon': -4.554390260747908,
-          'alt': 20
-        },
-        {
-          'type': 'waypoint',
-          'lat': 36.74696936877832,
-          'lon': -4.55462526862803,
-          'alt': 20
-        },
-        {
-          'type': 'waypoint',
-          'lat': 36.74658737079311,
-          'lon': -4.553846385367393,
-          'alt': 20
-        }
-      ]}
-    ]
   
     return router;
   }
